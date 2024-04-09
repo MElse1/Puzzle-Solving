34a89616d4cbdf0a752c14bb4a5e333e5d28a269
MElse1 <156001285+MElse1@users.noreply.github.com>
2024-04-09 14:21:21 -0400

    I think it's done

--- Word Search Solver/Word Search Solver refactored.py
+++ Word Search Solver/Word Search Solver refactored.py
@@ -6,17 +6,6 @@ while NextWord != "Done!":
     WordList.append(NextWord.lower())
     NextWord = input('Enter word to be added to search list, enter "Done!", without the quotation marks when complete:')
 ## Build a dummy array to test word finding logic
-DummyArrayofArrays = []
-DummyArray1 = ["a", "b", "c", "d", "e"]
-DummyArray2 = ["f", "g", "h", "i", "j"]
-DummyArray3 = ["k", "l", "m", "n", "o"]
-DummyArray4 = ["p", "q", "r", "s", "t"]
-DummyArray5 = ["u", "v", "w", "x", "y"]
-DummyArrayofArrays.append(DummyArray1)
-DummyArrayofArrays.append(DummyArray2)
-DummyArrayofArrays.append(DummyArray3)
-DummyArrayofArrays.append(DummyArray4)
-DummyArrayofArrays.append(DummyArray5)
 ## Get Possible lengths of words to search for
 WordListLen = []
 for Word in WordList:
@@ -25,8 +14,8 @@ for Word in WordList:
         WordListLen.append(l)
 RevStrSrch = ""
 StrSrch = ""
-xdim = len(DummyArrayofArrays[0]) ## determine X dimension by the length of the first array
-ydim = len(DummyArrayofArrays) ## determine Y dimension by the length of the array of arrays
+xdim = len(dictofstrings[0]) ## determine X dimension by the length of the first array
+ydim = len(dictofstrings) ## determine Y dimension by the length of the array of arrays
 for WordLen in WordListLen: ## For each length of word on the word list, important for determining edges of grid
     for xloc in range(xdim): 
         for yloc in range(ydim): 
@@ -35,8 +24,8 @@ for WordLen in WordListLen: ## For each length of word on the word list, importa
                 LSrch = "" # reset string search variables for each loop
                 RSrch = ""
                 while c < WordLen: 
-                    LSrch = LSrch + DummyArrayofArrays[yloc][xloc - c + WordLen - 1] # for horizontal and vertical searches we can search forward and backwards at the same tim
-                    RSrch = RSrch + DummyArrayofArrays[yloc][xloc + c] 
+                    LSrch = LSrch + dictofstrings[yloc][xloc - c + WordLen - 1] # for horizontal and vertical searches we can search forward and backwards at the same tim
+                    RSrch = RSrch + dictofstrings[yloc][xloc + c] 
                     if RSrch in WordList and len(RSrch) == WordLen: # if the search string matches a term on the list, the length check is to prevent double matches like cap/cape
                         print(RSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going right!")    #! debating if I should reverse the coordinates, because as is (0,0) is the top right
                     if LSrch in WordList and len(LSrch) == WordLen:                             #! and most people would picture (0,0) as the bottom right point.
@@ -47,8 +36,8 @@ for WordLen in WordListLen: ## For each length of word on the word list, importa
                 USrch = "" 
                 DSrch = ""
                 while c < WordLen: 
-                    USrch = USrch + DummyArrayofArrays[yloc - c + WordLen - 1][xloc] 
-                    DSrch = DSrch + DummyArrayofArrays[yloc + c][xloc]
+                    USrch = USrch + dictofstrings[yloc - c + WordLen - 1][xloc] 
+                    DSrch = DSrch + dictofstrings[yloc + c][xloc]
                     if DSrch in WordList and len(DSrch) == WordLen:
                         print(DSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going down!")
                     if USrch in WordList and len(USrch) == WordLen:
@@ -58,7 +47,7 @@ for WordLen in WordListLen: ## For each length of word on the word list, importa
                 c = 0 
                 DRSrch = ""
                 while c < WordLen:
-                    DRSrch = DRSrch + DummyArrayofArrays[yloc + c][xloc + c]
+                    DRSrch = DRSrch + dictofstrings[yloc + c][xloc + c]
                     if DRSrch in WordList and len(DRSrch) == WordLen:
                         print(DRSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going down and right!")
                     c += 1
@@ -66,7 +55,7 @@ for WordLen in WordListLen: ## For each length of word on the word list, importa
                 c = 0
                 ULSrch = "" 
                 while c < WordLen:
-                    ULSrch = ULSrch + DummyArrayofArrays[yloc - c][xloc - c]
+                    ULSrch = ULSrch + dictofstrings[yloc - c][xloc - c]
                     if ULSrch in WordList and len(ULSrch) == WordLen:
                         print(ULSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going up and left!")
                     c += 1
@@ -74,7 +63,7 @@ for WordLen in WordListLen: ## For each length of word on the word list, importa
                 c = 0
                 DLSrch = ""
                 while c < WordLen:
-                    DLSrch = DLSrch + DummyArrayofArrays[yloc + c][xloc - c]
+                    DLSrch = DLSrch + dictofstrings[yloc + c][xloc - c]
                     if DLSrch in WordList and len(DLSrch) == WordLen:
                         print(DLSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going up and right!")
                     c += 1
@@ -82,7 +71,7 @@ for WordLen in WordListLen: ## For each length of word on the word list, importa
                 c = 0
                 URSrch = ""
                 while c < WordLen:
-                    URSrch = URSrch + DummyArrayofArrays[yloc - c][xloc + c]
+                    URSrch = URSrch + dictofstrings[yloc - c][xloc + c]
                     if URSrch in WordList and len(URSrch) == WordLen:
                         print(URSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going down and left!")
                     c += 1
