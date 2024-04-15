e6d9cdae2d0db1c76141052b57d9bc07853ae337
MElse1 <156001285+MElse1@users.noreply.github.com>
2024-04-15 08:27:53 -0400

    Added a dictionary word search solver

--- Word Search Solver/Dictionary Word Search Solver.py
+++ Word Search Solver/Dictionary Word Search Solver.py
@@ -1,14 +1,14 @@
 import OCR_Letter_Grid as OCR
-## Starting with things i know how to do that won't require research
-## First gather list of words that need to be found
-WordList = []
-FoundWords = []
-NextWord =input('Enter word to be added to search list')
-while NextWord != "Done!":
-    NextWord.strip() # Remove whitespace
-    WordList.append(NextWord.upper()) # standardize all text to upper case
-    NextWord = input('Enter word to be added to search list, enter "Done!", without the quotation marks when complete:')
-## Build a dummy array to test word finding logic
+import enchant
+
+FoundWords = 0
+while True:
+    try:
+        MinLength = int(input("""The minimum length of word you want to find:"""))
+        break
+    except:
+        print("Please enter a number!  (IE: 4, 3, 12)")
+WordList = enchant.Dict("en_US")
 print("Enter the path to the picture of the letter grid")
 print("The easiest way to do this is to right click the file and select 'Copy as Path'")
 print("""Make sure you remove 's or "s from the path, we just need the path (IE: C:\lettergrid.png)""")
@@ -16,16 +16,9 @@ x = input()
 listofstrings = []
 listofstrings = OCR.OCRLG(x)
 ## Get Possible lengths of words to search for
-WordListLen = []
-for Word in WordList:
-    l = len(Word)
-    if l not in WordListLen: ## deduplicate list
-        WordListLen.append(l)
-RevStrSrch = ""
-StrSrch = ""
 xdim = len(listofstrings[0]) ## determine X dimension by the length of the first array
 ydim = len(listofstrings) ## determine Y dimension by the length of the array of arrays
-for WordLen in WordListLen: ## For each length of word on the word list, important for determining edges of grid
+for WordLen in range(MinLength,max(ydim,xdim)): ## For each length of word from the minimum length desired, to the largest dimension of the grid
     for xloc in range(xdim): 
         for yloc in range(ydim):
             if xloc + WordLen <= xdim: # horizontal search determine if the string will extend beyond the bounds of the grid
@@ -33,14 +26,14 @@ for WordLen in WordListLen: ## For each length of word on the word list, importa
                 LSrch = "" # reset string search variables for each loop
                 RSrch = ""
                 while c < WordLen: 
-                    LSrch = LSrch + listofstrings[yloc][xloc - c + WordLen - 1] # for horizontal and vertical searches we can search forward and backwards at the same tim
+                    LSrch = LSrch + listofstrings[yloc][xloc - c + WordLen - 1] # for horizontal and vertical searches we can search forward and backwards at the same time
                     RSrch = RSrch + listofstrings[yloc][xloc + c] 
-                    if RSrch in WordList and len(RSrch) == WordLen: # if the search string matches a term on the list, the length check is to prevent double matches like cap/cape
-                        FoundWords.append(RSrch)
-                        print(RSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going right!")    #! debating if I should reverse the Y coordinates, because as is (0,0) is the top left
-                    if LSrch in WordList and len(LSrch) == WordLen:                             #! and most people would picture (0,0) as the bottom right left.
-                        FoundWords.append(LSrch)
+                    if len(RSrch) == WordLen and WordList.check(RSrch): # if the search string matches a term on the list, the length check is to prevent double matches like cap/cape
+                        print(RSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going right!")
+                        FoundWords += 1                                                              #! debating if I should reverse the Y coordinates, because as is (1,1) is the top left
+                    if len(LSrch) == WordLen and WordList.check(LSrch):                              #! and most people would picture (1,1) as the bottom right.
                         print(LSrch, "located at x:",xloc + WordLen,"y:",yloc + 1, "going left!")
+                        FoundWords += 1
                     c += 1
             if yloc + WordLen <= ydim: # vertical search determine if the string will extend beyond the bounds of the grid
                 c = 0 
@@ -49,54 +42,55 @@ for WordLen in WordListLen: ## For each length of word on the word list, importa
                 while c < WordLen: 
                     USrch = USrch + listofstrings[yloc - c + WordLen - 1][xloc] 
                     DSrch = DSrch + listofstrings[yloc + c][xloc]
-                    if DSrch in WordList and len(DSrch) == WordLen:
-                        FoundWords.append(DSrch)
+                    if len(DSrch) == WordLen and WordList.check(DSrch):
                         print(DSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going down!")
-                    if USrch in WordList and len(USrch) == WordLen:
-                        FoundWords.append(USrch)
+                        FoundWords += 1
+                    if len(USrch) == WordLen and WordList.check(USrch):
                         print(USrch, "located at x:",xloc + 1,"y:",yloc + WordLen, "going up!")
+                        FoundWords += 1
                     c += 1
             if yloc + WordLen <= ydim and xloc + WordLen <= xdim: # diagonal search determine if the string will extend beyond the bounds of the grid
                 c = 0 
                 DRSrch = ""
                 while c < WordLen:
                     DRSrch = DRSrch + listofstrings[yloc + c][xloc + c]
-                    if DRSrch in WordList and len(DRSrch) == WordLen:
-                        FoundWords.append(DRSrch)
+                    if len(DRSrch) == WordLen and WordList.check(DRSrch):
                         print(DRSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going down and right!")
+                        FoundWords += 1
                     c += 1
             if yloc + 1 - WordLen >= 0 and xloc + 1 - WordLen >= 0:
                 c = 0
                 ULSrch = "" 
                 while c < WordLen:
                     ULSrch = ULSrch + listofstrings[yloc - c][xloc - c]
-                    if ULSrch in WordList and len(ULSrch) == WordLen:
-                        FoundWords.append(ULSrch)
+                    if len(ULSrch) == WordLen and WordList.check(ULSrch):
                         print(ULSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going up and left!")
+                        FoundWords += 1
                     c += 1
             if yloc + WordLen <= ydim and xloc + 1 - WordLen >= 0:
                 c = 0
                 DLSrch = ""
                 while c < WordLen:
                     DLSrch = DLSrch + listofstrings[yloc + c][xloc - c]
-                    if DLSrch in WordList and len(DLSrch) == WordLen:
-                        FoundWords.append(DLSrch)
+                    if len(DLSrch) == WordLen and WordList.check(DLSrch):
                         print(DLSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going up and right!")
+                        FoundWords += 1
                     c += 1
             if yloc + 1 - WordLen >= 0 and xloc + WordLen <= xdim:
                 c = 0
                 URSrch = ""
                 while c < WordLen:
                     URSrch = URSrch + listofstrings[yloc - c][xloc + c]
-                    if URSrch in WordList and len(URSrch) == WordLen:
-                        FoundWords.append(URSrch)
+                    if len(URSrch) == WordLen and WordList.check(URSrch):
                         print(URSrch, "located at x:",xloc + 1,"y:",yloc + 1, "going down and left!")
+                        FoundWords += 1
                     c += 1
+print("Found", FoundWords, "total english words longer than", MinLength)                    
 print("Note that coordinate x: 1, y: 1 is the top left corner")
-if len(WordList) == len(FoundWords):
-    print("All words located!")
-else:
-    print(len(WordList) - len(FoundWords),"Words not found, They are:")
-    for word in WordList:
-        if word not in FoundWords:
-            print(word)
+##if len(WordList) == len(FoundWords):
+##    print("All words located!")
+##else:
+##    print(len(WordList) - len(FoundWords),"Words not found, They are:")
+##    for word in WordList:
+##        if word not in FoundWords:
+##            print(word)
